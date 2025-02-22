c5387dcc4a1b65640c3e2e2250f85b959f2df54f
matteomarjanovic <matteomarjanovic@gmail.com>
2025-02-22 18:33:15 +0200

    remove umami script from layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,12 +3,4 @@
 	let { children } = $props();
 </script>
 
-<svelte:head>
-	<script
-		defer
-		src="https://mm-umami.netlify.app/script.js"
-		data-website-id="b80ba154-c32c-47aa-b133-6ab977537dbf"
-	></script>
-</svelte:head>
-
 {@render children()}
